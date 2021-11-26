864409c99bc76dcfd742d278e9636ee34536b8cc
Aziz Sobirov <azizsobirovdev@gmail.com>
2021-11-26 11:43:50 +0500

    12

---
+++ src/components/TheReferences.vue
@@ -0,0 +1,24 @@
+<template>
+    <span>
+        <router-link to="/"
+          ><img
+            src="https://wtt-solutions.com/images/socialNetwork/stars.svg"
+            alt="img" /><img
+            src="https://wtt-solutions.com/images/socialNetwork/clutch.svg"
+            alt="img"
+        /></router-link>
+        <router-link to="/"
+          ><p>TOP RATED PLUS</p>
+          <img
+            src="https://wtt-solutions.com/images/socialNetwork/upwork.svg"
+            alt="img"
+        /></router-link>
+        <router-link to="/"
+          ><img
+            src="https://wtt-solutions.com/images/socialNetwork/stars.svg"
+            alt="img" /><img
+            src="https://wtt-solutions.com/images/socialNetwork/goodfirms.svg"
+            alt="img"
+        /></router-link>
+    </span>
+</template>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -25,26 +25,7 @@
         />
       </div>
       <div class="home_rates">
-        <router-link to="/"
-          ><img
-            src="https://wtt-solutions.com/images/socialNetwork/stars.svg"
-            alt="img" /><img
-            src="https://wtt-solutions.com/images/socialNetwork/clutch.svg"
-            alt="img"
-        /></router-link>
-        <router-link to="/"
-          ><p>TOP RATED PLUS</p>
-          <img
-            src="https://wtt-solutions.com/images/socialNetwork/upwork.svg"
-            alt="img"
-        /></router-link>
-        <router-link to="/"
-          ><img
-            src="https://wtt-solutions.com/images/socialNetwork/stars.svg"
-            alt="img" /><img
-            src="https://wtt-solutions.com/images/socialNetwork/goodfirms.svg"
-            alt="img"
-        /></router-link>
+        <references/>
       </div>
       <a href="#services" class="home_mouse"></a>
     </section>
@@ -66,30 +47,61 @@
           ></router-link>
         </li>
       </ul>
-      <div class="services_swiper">
-          <button @click="prev()" :disabled='prevBtnDisabled'><i class="ai-circle-chevron-left"></i
+      <div id="swiper">
+          <button @click="servicePrev()" :disabled='prevBtnDisabled'><i class="ai-circle-chevron-left"></i
           ></button>
           <div class="sliders">
-              <div :class="slider" v-for="index in services" :key='index' @click='slideTo(index.id)'></div>
+              <div class="slider" v-for="index in services" :key='index' @click='serviceSlideTo(index.id)'></div>
           </div>
-          <button @click="next()"  :disabled='nextBtnDisabled'><i class="ai-circle-chevron-right"></i
+          <button @click="serviceNext()"  :disabled='nextBtnDisabled'><i class="ai-circle-chevron-right"></i
           ></button>
       </div>
     </section>
 
+    <section class="clients">
+      <h2>WHAT OUR CLIENTS ARE SAYING</h2>
+      <ul>
+        <li v-for="client in clients" :key="client">
+          <div class="clients_text">
+            <img :src="client.img" :alt="client.name">
+            <span>
+              <h3>{{client.name}}</h3>
+              <p>{{client.company}}</p>
+              <b>{{client.hobby}}</b>
+            </span>
+          </div>
+          <p>{{client.description}}</p>
+          <a :href="client.href">View full review <i class="ai-arrow-right"></i></a>
+        </li>
+      </ul>
+      <div class="clients_content">
+        <references/>
+        <div id="swiper">
+          <button @click="clientsPrev()" ><i class="ai-circle-chevron-left"></i
+          ></button>
+          <div class="sliders">
+              <div class="slider" v-for="index in clients" :key='index.id' @click='clientsSlideTo(index.id)'></div>
+          </div>
+          <button @click="clientsNext()"><i class="ai-circle-chevron-right"></i
+          ></button>
+        </div>
+      </div>
+    </section>
 
   </section>
 </template>
 
 <script>
+import references from '../components/TheReferences.vue'
 export default {
   name: "Home",
+  components: {
+    references
+  },
   data() {
     return {
-        translate:0,
-        slider:'slider',
-        nextBtnDisabled: false,
-        prevBtnDisabled: false,
+        serviceSlide:0,
+        clientsSlide:0,
       services: [
         {
           id:0,
@@ -128,33 +140,85 @@ export default {
           href: "enterprise-dev",
         },
       ],
+      clients:[
+        {
+          id:0,
+          img:'https://wtt-solutions.com/_next/image?url=%2Fimages%2Freviews%2FMichaelKauffman.jpg&w=1200&q=75',
+          name:'MICHAEL KAUFFMAN',
+          company:'Werkcrush',
+          hobby:'Founder',
+          description:"WTT Solutions has integrated APIs, improved user experience, and launched a working platform. The updated platform has become more convenient and scalable. Clear communication and a high level of knowledge were the hallmarks of this collaboration. The work of WTT will allow us to launch and scale a business much faster and more efficiently.",
+          href:'#'
+        },{
+          id:1,
+          img:'https://wtt-solutions.com/_next/image?url=%2Fimages%2Freviews%2FMichaelKauffman.jpg&w=1200&q=75',
+          name:'MICHAEL KAUFFMAN',
+          company:'Werkcrush',
+          hobby:'Founder',
+          description:"WTT Solutions has integrated APIs, improved user experience, and launched a working platform. The updated platform has become more convenient and scalable. Clear communication and a high level of knowledge were the hallmarks of this collaboration. The work of WTT will allow us to launch and scale a business much faster and more efficiently.",
+          href:'#'
+        },{
+          id:2,
+          img:'https://wtt-solutions.com/_next/image?url=%2Fimages%2Freviews%2FMichaelKauffman.jpg&w=1200&q=75',
+          name:'MICHAEL KAUFFMAN',
+          company:'Werkcrush',
+          hobby:'Founder',
+          description:"WTT Solutions has integrated APIs, improved user experience, and launched a working platform. The updated platform has become more convenient and scalable. Clear communication and a high level of knowledge were the hallmarks of this collaboration. The work of WTT will allow us to launch and scale a business much faster and more efficiently.",
+          href:'#'
+        },{
+          id:3,
+          img:'https://wtt-solutions.com/_next/image?url=%2Fimages%2Freviews%2FMichaelKauffman.jpg&w=1200&q=75',
+          name:'MICHAEL KAUFFMAN',
+          company:'Werkcrush',
+          hobby:'Founder',
+          description:"WTT Solutions has integrated APIs, improved user experience, and launched a working platform. The updated platform has become more convenient and scalable. Clear communication and a high level of knowledge were the hallmarks of this collaboration. The work of WTT will allow us to launch and scale a business much faster and more efficiently.",
+          href:'#'
+        },{
+          id:4,
+          img:'https://wtt-solutions.com/_next/image?url=%2Fimages%2Freviews%2FMichaelKauffman.jpg&w=1200&q=75',
+          name:'MICHAEL KAUFFMAN',
+          company:'Werkcrush',
+          hobby:'Founder',
+          description:"WTT Solutions has integrated APIs, improved user experience, and launched a working platform. The updated platform has become more convenient and scalable. Clear communication and a high level of knowledge were the hallmarks of this collaboration. The work of WTT will allow us to launch and scale a business much faster and more efficiently.",
+          href:'#'
+        },{
+          id:5,
+          img:'https://wtt-solutions.com/_next/image?url=%2Fimages%2Freviews%2FMichaelKauffman.jpg&w=1200&q=75',
+          name:'MICHAEL KAUFFMAN',
+          company:'Werkcrush',
+          hobby:'Founder',
+          description:"WTT Solutions has integrated APIs, improved user experience, and launched a working platform. The updated platform has become more convenient and scalable. Clear communication and a high level of knowledge were the hallmarks of this collaboration. The work of WTT will allow us to launch and scale a business much faster and more efficiently.",
+          href:'#'
+        }
+      ]
     };
   },
   mounted() {
-    this.sliderActive(0)
+    this.service_sliderActive(0),
+    this.clients_sliderActive(0)
   },
   methods: {
-    next(){
-      if(this.translate === this.services.length - 1){
-        this.translate = 0
+    serviceNext(){
+      if(this.serviceSlide === this.services.length - 1){
+        this.serviceSlide = 0
       }else{
-        this.translate++
+        this.serviceSlide++
       }
-      this.sliderActive(this.translate)
+      this.service_sliderActive(this.serviceSlide)
     },
-    prev(){
-      if(this.translate === 0){
-        this.translate = this.services.length - 1
+    servicePrev(){
+      if(this.serviceSlide === 0){
+        this.serviceSlide = this.services.length - 1
       }else{
-        this.translate--
+        this.serviceSlide--
       }
-      this.sliderActive(this.translate)
+      this.service_sliderActive(this.serviceSlide)
     },
-    slideTo(index){
-      this.translate = index
-      this.sliderActive(this.translate)
+    serviceSlideTo(index){
+      this.serviceSlide = index
+      this.service_sliderActive(this.serviceSlide)
     },
-    sliderActive(index){
+    service_sliderActive(index){
       let sliders = document.querySelectorAll(".services .slider")
       let service = document.querySelectorAll('.services ul li')
       for(let i=0;i<this.services.length;i++){
@@ -166,11 +230,75 @@ export default {
           service[i].style.transform='rotateX(25deg)'
         }
       }
-      this.slide()
+      this.servicesSlide()
     },
-    slide(){
+    servicesSlide(){
       let el = document.querySelector('.services ul')
-      el.style.transform=`translateX(-${this.translate}00%)`
+      el.style.transform=`translateX(-${this.serviceSlide}00%)`
+    },
+
+// clients swiper
+
+    clientsNext(){
+      if(window.innerWidth <= '1024'){
+        if(this.clientsSlide === this.clients.length - 1){
+          this.clientsSlide = 0
+        }else{
+         this.clientsSlide++
+        }
+      }else{
+        if(this.clientsSlide === this.clients.length - 2){
+          this.clientsSlide = 0
+        }else{
+         this.clientsSlide++
+        }
+      }
+      
+      this.clients_sliderActive(this.clientsSlide)
+    },
+    clientsPrev(){
+      if(window.innerWidth <= '1024'){
+        if(this.clientsSlide === 0){
+          this.clientsSlide = this.clients.length - 2
+        }else{
+          this.clientsSlide--
+        }      
+      }else{
+        if(this.clientsSlide === 0){
+          this.clientsSlide = this.clients.length - 1
+        }else{
+          this.clientsSlide--
+        }
+      }
+
+      this.clients_sliderActive(this.clientsSlide)
+    },
+    clientsSlideTo(index){
+      this.clientsSlide = index
+      this.service_sliderActive(this.clientsSlide)
+    },
+    clients_sliderActive(index){
+      let sliders = document.querySelectorAll(".clients .slider")
+      let service = document.querySelectorAll('.clients ul li')
+      for(let i=0;i<this.clients.length;i++){
+        if(i === index){
+          sliders[i].style.background="#1ba94c"
+          service[i].style.transform='rotateX(0deg)'
+        }else{
+          sliders[i].style.background='#777'
+          service[i].style.transform='rotateX(25deg)'
+        }
+      }
+      this.clientSlide()
+    },
+    clientSlide(){
+      let el = document.querySelector('.clients ul')
+      if(window.innerWidth <= '1024'){
+        el.style.transform=`translateX(-${this.clientsSlide+'00'}%)`
+      }else{
+        let scrollTo = this.clientsSlide / 2 * 10
+        el.style.transform=`translateX(-${scrollTo+'0'}%)`
+      }
     },
   },
 };
